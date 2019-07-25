a91947310b0a3518d9b9fdd0878ef8bd16887eeb
blueberry20 <olgasales@gmail.com>
2019-07-25 00:05:58 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+// template can have only one root
+<template>
+  <div id="app">
+    <SearchBar></SearchBar>
+  </div>
+</template>
+
+<script>
+import SearchBar from './components/searchBar.vue'
+
+export default {
+//use name for debugging tools
+//data, methods and computed go here
+  name: 'app',
+  components: {
+    SearchBar: SearchBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

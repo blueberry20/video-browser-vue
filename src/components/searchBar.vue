c9b3945df1b9b39016989e2cf6dce9ccb737c0d6
blueberry20 <olgasales@gmail.com>
2019-07-25 00:11:28 -0400

    wired onInput event handler in SearchBar

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
-        <input/>
+        <input v-on:input="onInput" />
     </div>
 </template>
 
 <script>
     export default {
-        name: "SearchBar"
+        name: "SearchBar",
+        //methods give us an opportunity to change data
+        methods: {
+            onInput: function(event){
+                console.log(event.target.value);
+            }
+        }
     }
 </script>
 

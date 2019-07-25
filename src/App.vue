b7c1dcc73288f2cc09ac3092e6083ffa8ece4e38
Olga Smith <olgasales@gmail.com>
2019-07-25 15:36:39 -0400

    working app. on video click, the title is displayed in the video detail

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,58 @@
 // template can have only one root
 <template>
   <div id="app">
-    <SearchBar></SearchBar>
+    <!-- v-on: = @ -->
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoDetail v-bind:video="selectedVideo" />
+    <VideoList @videoSelect="onVideoSelect" v-bind:videoList="videos"/> 
+    <!-- pass a prop to the child. videos is the name of the prop and videos is data from this component -->
+    <!-- v-bind: if videos are updated, the child is rerendered -->
+    <!-- {{videos.length}} -->
   </div>
 </template>
 
 <script>
-import SearchBar from './components/searchBar.vue'
+import axios from 'axios';
+import SearchBar from './components/searchBar.vue';
+import VideoList from './components/videoList';
+import VideoDetail from './components/videoDetail';
+const API_KEY = 'AIzaSyAgCpmzDnH5XX1fVpTi7VI9SrHrEBYPzkE';
+// import TextField from './components/textField.vue';
 
 export default {
 //use name for debugging tools
 //data, methods and computed go here
   name: 'app',
   components: {
-    SearchBar: SearchBar
+    SearchBar: SearchBar,
+    VideoList,
+    VideoDetail 
+    // TextField: TextField
+  },
+  data: function(){ //this is a vue component, not an instance. it's a function not an object
+    return {
+      //initialize our data
+      videos: [],
+      selectedVideo: null
+    }
+  },
+  methods: {
+    onTermChange(searchTerm){
+      axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet', //what kind of info we want back
+          q: searchTerm //what we are looking for
+        }
+      }).then(response =>{
+        //update data 
+        this.videos = response.data.items;
+      })
+    },
+    onVideoSelect(video){
+      this.selectedVideo = video;
+    }
   }
 }
 </script>
